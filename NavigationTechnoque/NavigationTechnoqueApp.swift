6eaf8242a67c4d4821b00f540246862d8ff39cf2
isiflo2728 <floresisidoro30@gmail.com>
2026-04-29 17:55:34 -0700

    Initial Commit

---
+++ NavigationTechnoque/NavigationTechnoqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTechnoqueApp.swift
+//  NavigationTechnoque
+//
+//  Created by Isidoro Flores on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTechnoqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigationTechnoqueTests/NavigationTechnoqueTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTechnoqueTests.swift
+//  NavigationTechnoqueTests
+//
+//  Created by Isidoro Flores on 4/29/26.
+//
+
+import Testing
+@testable import NavigationTechnoque
+
+struct NavigationTechnoqueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
